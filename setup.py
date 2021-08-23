301af9d3042a3a34fb50c04135c874f585e46829
Adam Coddington <me@adamcoddington.net>
2021-08-22 17:31:07 -0700

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name="safdie",
-    version="1.0.0",
+    version="1.0.1",
     license="MIT",
     description=(
         "Easily make your app extensible by you or others via use of "
